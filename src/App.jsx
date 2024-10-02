9fb4087582a365444df898fcb6e64b612b248ab3
AdnanShaikh76 <adushaikh2001@gmail.com>
2024-10-02 15:15:28 +0530

    upload changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import React, { useState } from "react";
 import Task from "./components/task.";
 
 function App() {
-
   const [component, setcomponent] = useState([]);
-  
+
   const taskvalue = document.getElementById("taskvalue");
 
-  function removetask(){
-    component.pop()
-}
-  
-  function addcomponent(){
-    setcomponent([...component, <Task key={component.length} />])
+  const addComponent = () => {
+    setcomponent([...component, <Task key={component.length} />]);
     console.log(taskvalue)
-  }
-
+    taskvalue.innerText = "";
+  };
 
   return (
     <>
@@ -23,15 +18,23 @@ function App() {
         <header className="w-full h-20 bg-slate-500 text-yellow-200 text-3xl flex items-center justify-center">
           To-Do Application
         </header>
-        <input type="text" name="task" id="taskvalue" className="py-3 px-3 w-7/12 mx-32" placeholder="Enter Your Task " />
+        <input
+          type="text"
+          name="task"
+          id="taskvalue"
+          className="py-3 px-3 w-7/12 mx-32"
+          placeholder="Enter Your Task "
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
         <button
           className="px-5 py-3 bg-slate-600 rounded-md text-yellow-200 mx-6 my-5"
-          onClick={addcomponent}
-        >Add Task
+          onClick={addComponent}
+        >
+          Add Task
         </button>
         <div className="w-full min-h-screen bg-slate-300 flex flex-col items-center py-8">
-          
-
           {component}
         </div>
       </div>
--- src/components/task..jsx
+++ src/components/task..jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 
-function Task(props) {
+function Task({ taskvalue}) {
 
-
+  const removetask = function removetask(id){
+    setComponents(components.filter((component) => component.id !== id));
+}  
     
   return (
     <div className='w-10/12 bg-slate-400 px-4  text-slate-900 uppercase my-3 flex gap-2 items-center justify-between'>
-        <div className='w-6/12 h-full py-5'>
-            <p className='text-slate-700 text-lg'>This is text</p>
+        <div className='w-6/12 h-full py-4'>
+            <p className='text-slate-700 text-lg'>{taskvalue}</p>
         </div>
         
         <button className='bg-white px-3 py-2'
